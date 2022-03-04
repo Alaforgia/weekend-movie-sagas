a05f692b8cf6444683a11423470760fbb48327de
Tony LaForgia <alforgia428@gmail.com>
2022-03-04 16:56:55 -0600

    Working on Details Genre Map

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import MovieList from "../MovieList/MovieList";
 
 function Details() {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
+  const movieDetailList = useSelector((store) => store.genre);
 
-
-  
-
+  // {movieDetailList.map(({list}))}
   return (
-    <div>
-      <button onClick={}>Back to Movies</button>
-    </div>
-  )
+    <>
+      <h1>Movie Details</h1>
+      <div>
+        {movieDetailList.map()}
+        <MovieList />
+        {/* <button onClick={}>Back to Movies</button> */}
+      </div>
+    </>
+  );
 }
 
-export default Details
+export default Details;
