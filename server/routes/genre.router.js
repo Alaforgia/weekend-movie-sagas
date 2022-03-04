fca1354923de848e661fa8306676c251fb77edc4
Tony LaForgia <alforgia428@gmail.com>
2022-03-04 16:34:49 -0600

    Created genre server route. Created genre saga

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+router.get("/", (req, res) => {
+  const query = `SELECT "movies"."title", "genres"."name", "movies"."description", "movies"."poster" 
+  FROM "movies"
+  JOIN "movies_genres" ON "movies"."id" = "movies_genres"."movie_id"
+  JOIN "genres" ON "movies_genres"."genre_id" = "genres"."id"
+  GROUP BY "movies"."title", "genres"."name", "movies"."description", "movies"."poster";`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Get Details", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
